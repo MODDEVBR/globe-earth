72b94327ae632edfcdc89689d0eeb0d1bd6ee0ef
andreocunha <andreoliveiracunha20@gmail.com>
2022-08-17 08:14:37 -0300

    earth

---
+++ data.js
@@ -0,0 +1,35 @@
+data = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Sao Paulo",
+        "description": "Alguma descricao legal aqui",
+        "latitude": -23.558679587,
+        "longitude": -46.6250199804,
+        "pop_max": 18845000,
+        "pop_min": 10021295
+      },
+      "bbox": [
+        -46.626966,
+        -23.556734,
+        -46.626966,
+        -23.556734
+      ],
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -46.626966,
+          -23.556734
+        ]
+      }
+    }
+  ],
+  "bbox": [
+    -175.220564477617,
+    -41.2999878536917,
+    179.216647094029,
+    64.1500236197392
+  ]
+}
