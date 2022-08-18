6d49ebdef2c3103b679a7d4d965518f0fca69665
andreocunha <andreoliveiracunha20@gmail.com>
2022-08-18 14:56:45 -0300

    novos pontos

--- data.js
+++ data.js
@@ -4,32 +4,211 @@ data = {
     {
       "type": "Feature",
       "properties": {
-        "name": "Sao Paulo",
-        "description": "Alguma descricao legal aqui",
-        "latitude": -23.558679587,
-        "longitude": -46.6250199804,
-        "pop_max": 18845000,
-        "pop_min": 10021295
-      },
-      "bbox": [
-        -46.626966,
-        -23.556734,
-        -46.626966,
-        -23.556734
-      ],
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -46.626966,
-          -23.556734
-        ]
+        "name": "ASN - Praia do Futuro Ellalink",
+        "description": "Levantamento hidrográfico e geofísico na Praia do Futuro, em Fortaleza/CE",
+        "latitude": -3.7796961,
+        "longitude": -38.4886897,
+        "pop_max": 4045000,
+        "pop_min": 3021295
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "DTS - VC Telecom",
+        "description": "Estudo de Viabilidade de Rota para implementação de cabo de telecomunicação",
+        "latitude": -1.8476142,
+        "longitude": -53.6085004,
+        "pop_max": 4045000,
+        "pop_min": 3021295
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "RNP - Infovia 00",
+        "description": "Estudo de Viabilidade e Levantamento hidrográfico e geofísico para implementação de cabo de telecomunicação",
+        "latitude": -0.090316,
+        "longitude": -51.0386419,
+        "pop_max": 4045000,
+        "pop_min": 3021295
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "RNP - Noronha",
+        "description": "Levantamento hidrográfico geofísico entre Natal e Fernando de Noronha",
+        "latitude": -5.7048034,
+        "longitude": -35.3427023,
+        "pop_max": 4045000,
+        "pop_min": 3021295
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "CELSE - Sergipe",
+        "description": "Levantamento hidrográfico em Barra dos Coqueiros, SE",
+        "latitude": -10.9262385,
+        "longitude": -37.0149954,
+        "pop_max": 4045000,
+        "pop_min": 3021295
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Telxius - SAM1",
+        "description": "Levantamento hidrográfico e geofísico em Salvador/BA",
+        "latitude": -13.3944184,
+        "longitude": -39.3318501,
+        "pop_max": 4045000,
+        "pop_min": 3021295
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Hydrobiology",
+        "description": "Levantamento hidrográfico, geotécnico, geofísico e ambiental na Foz do Rio Doce e área de influência",
+        "latitude": -19.6722886,
+        "longitude": -39.5294313,
+        "pop_max": 4045000,
+        "pop_min": 3021295
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Sede da EGS Brasil",
+        "description": "Sede da EGS Brasil",
+        "latitude": -20.1511615,
+        "longitude": -40.1077883,
+        "pop_max": 4045000,
+        "pop_min": 3021295
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Shore Approach - Cabiunas",
+        "description": "Levantamento topobatimétrico e oceanográfico em Cabiúnas, Macaé/RJ.",
+        "latitude": -22.3365114,
+        "longitude": -41.5309827,
+        "pop_max": 4045000,
+        "pop_min": 3021295
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Shore Approach - BDR",
+        "description": "Levantamento topobatimétrico e oceanográfico em Barra do Riacho, Aracruz/ES.",
+        "latitude": -19.8450101,
+        "longitude": -40.0396857,
+        "pop_max": 4045000,
+        "pop_min": 3021295
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Petrobras - Jaconé",
+        "description": "Levantamento topobatimétrico e meteoceanográfico em Jaconé, Maricá/RJ.",
+        "latitude": -22.95052,
+        "longitude": -42.6214652,
+        "pop_max": 4045000,
+        "pop_min": 3021295
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Subcom - Santos",
+        "description": "Levantamento hidrográfico, topográfico e geofísico em Praia Grande, Santos/SP",
+        "latitude": -23.6635092,
+        "longitude": -45.5014559,
+        "pop_max": 4045000,
+        "pop_min": 3021295
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "PUC - Sismica",
+        "description": "Levantamento sísmico multicanal de altíssima resolução na região da Elevação do Rio Grande, RS.",
+        "latitude": -29.6518458,
+        "longitude": -36.2189962,
+        "pop_max": 4045000,
+        "pop_min": 3021295
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Subcom - Las Toninas",
+        "description": "Levantamento hidrográfico, topográfico e geofísico em Las Toninas, Buenos Aires, Argentina.",
+        "latitude": -36.490158,
+        "longitude": -56.4530812,
+        "pop_max": 4045000,
+        "pop_min": 3021295
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Malbec - Macumba",
+        "description": "Levantamento hidrográfico, topográfico e geofísico na Praia da Macumba, Rio de Janeiro/RJ.",
+        "latitude": -23.0394727,
+        "longitude": -43.4695563,
+        "pop_max": 4045000,
+        "pop_min": 3021295
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Site Visit TCFS",
+        "description": "Levantamento topográfico e inspeção visual subaquática em San Juan, Porto Rico",
+        "latitude": 18.1865566,
+        "longitude": -65.113287,
+        "pop_max": 4045000,
+        "pop_min": 3021295
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "TCFS - USVI 1",
+        "description": "Levantamento topográfico e inspeção visual subaquática em Saint Thomas, Ilhas Virgens Americanas",
+        "latitude": 18.29802,
+        "longitude": -64.9044384,
+        "pop_max": 4045000,
+        "pop_min": 3021295
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "TCFS - USVI 2",
+        "description": "Levantamento topográfico e inspeção visual subaquática em Saint Croix, Ilhas Virgens Americanas",
+        "latitude": 17.7222475,
+        "longitude": -64.7958542,
+        "pop_max": 4045000,
+        "pop_min": 3021295
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "3R - Guamaré",
+        "description": "Monitoramento da morfodinâmica costeira da Praia de Soledade, RN.",
+        "latitude": -5.3050891,
+        "longitude": -36.4960709,
+        "pop_max": 4045000,
+        "pop_min": 3021295
       }
     }
-  ],
-  "bbox": [
-    -175.220564477617,
-    -41.2999878536917,
-    179.216647094029,
-    64.1500236197392
   ]
 }
