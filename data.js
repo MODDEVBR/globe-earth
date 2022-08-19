b0ba698b7aa42c90fbfa3b38ff3a4a3ce1f57ee3
andreocunha <andreoliveiracunha20@gmail.com>
2022-08-19 09:52:43 -0300

    info globo

--- data.js
+++ data.js
@@ -8,7 +8,7 @@ data = {
         "description": "Levantamento hidrográfico e geofísico na Praia do Futuro, em Fortaleza/CE",
         "latitude": -3.7796961,
         "longitude": -38.4886897,
-        "pop_max": 1245000,
+        "pop_max": 1545000,
         "pop_min": 1021295
       }
     },
@@ -19,7 +19,7 @@ data = {
         "description": "Estudo de Viabilidade de Rota para implementação de cabo de telecomunicação",
         "latitude": -1.8476142,
         "longitude": -53.6085004,
-        "pop_max": 1245000,
+        "pop_max": 1545000,
         "pop_min": 1021295
       }
     },
@@ -30,7 +30,7 @@ data = {
         "description": "Estudo de Viabilidade e Levantamento hidrográfico e geofísico para implementação de cabo de telecomunicação",
         "latitude": -0.090316,
         "longitude": -51.0386419,
-        "pop_max": 1245000,
+        "pop_max": 1545000,
         "pop_min": 1021295
       }
     },
@@ -41,7 +41,7 @@ data = {
         "description": "Levantamento hidrográfico geofísico entre Natal e Fernando de Noronha",
         "latitude": -5.7048034,
         "longitude": -35.3427023,
-        "pop_max": 1245000,
+        "pop_max": 1545000,
         "pop_min": 1021295
       }
     },
@@ -52,7 +52,7 @@ data = {
         "description": "Levantamento hidrográfico em Barra dos Coqueiros, SE",
         "latitude": -10.9262385,
         "longitude": -37.0149954,
-        "pop_max": 1245000,
+        "pop_max": 1545000,
         "pop_min": 1021295
       }
     },
@@ -63,7 +63,7 @@ data = {
         "description": "Levantamento hidrográfico e geofísico em Salvador/BA",
         "latitude": -13.3944184,
         "longitude": -39.3318501,
-        "pop_max": 1245000,
+        "pop_max": 1545000,
         "pop_min": 1021295
       }
     },
@@ -74,7 +74,7 @@ data = {
         "description": "Levantamento hidrográfico, geotécnico, geofísico e ambiental na Foz do Rio Doce e área de influência",
         "latitude": -19.6722886,
         "longitude": -39.5294313,
-        "pop_max": 1245000,
+        "pop_max": 1545000,
         "pop_min": 1021295
       }
     },
@@ -85,7 +85,7 @@ data = {
         "description": "Sede da EGS Brasil",
         "latitude": -20.1511615,
         "longitude": -40.1077883,
-        "pop_max": 1245000,
+        "pop_max": 1545000,
         "pop_min": 1021295
       }
     },
@@ -96,7 +96,7 @@ data = {
         "description": "Levantamento topobatimétrico e oceanográfico em Cabiúnas, Macaé/RJ.",
         "latitude": -22.3365114,
         "longitude": -41.5309827,
-        "pop_max": 1245000,
+        "pop_max": 1545000,
         "pop_min": 1021295
       }
     },
@@ -107,7 +107,7 @@ data = {
         "description": "Levantamento topobatimétrico e oceanográfico em Barra do Riacho, Aracruz/ES.",
         "latitude": -19.8450101,
         "longitude": -40.0396857,
-        "pop_max": 1245000,
+        "pop_max": 1545000,
         "pop_min": 1021295
       }
     },
@@ -118,7 +118,7 @@ data = {
         "description": "Levantamento topobatimétrico e meteoceanográfico em Jaconé, Maricá/RJ.",
         "latitude": -22.95052,
         "longitude": -42.6214652,
-        "pop_max": 1245000,
+        "pop_max": 1545000,
         "pop_min": 1021295
       }
     },
@@ -129,7 +129,7 @@ data = {
         "description": "Levantamento hidrográfico, topográfico e geofísico em Praia Grande, Santos/SP",
         "latitude": -23.6635092,
         "longitude": -45.5014559,
-        "pop_max": 1245000,
+        "pop_max": 1545000,
         "pop_min": 1021295
       }
     },
@@ -140,7 +140,7 @@ data = {
         "description": "Levantamento sísmico multicanal de altíssima resolução na região da Elevação do Rio Grande, RS.",
         "latitude": -29.6518458,
         "longitude": -36.2189962,
-        "pop_max": 1245000,
+        "pop_max": 1545000,
         "pop_min": 1021295
       }
     },
@@ -151,7 +151,7 @@ data = {
         "description": "Levantamento hidrográfico, topográfico e geofísico em Las Toninas, Buenos Aires, Argentina.",
         "latitude": -36.490158,
         "longitude": -56.4530812,
-        "pop_max": 1245000,
+        "pop_max": 1545000,
         "pop_min": 1021295
       }
     },
@@ -162,7 +162,7 @@ data = {
         "description": "Levantamento hidrográfico, topográfico e geofísico na Praia da Macumba, Rio de Janeiro/RJ.",
         "latitude": -23.0394727,
         "longitude": -43.4695563,
-        "pop_max": 1245000,
+        "pop_max": 1545000,
         "pop_min": 1021295
       }
     },
@@ -173,7 +173,7 @@ data = {
         "description": "Levantamento topográfico e inspeção visual subaquática em San Juan, Porto Rico",
         "latitude": 18.1865566,
         "longitude": -65.113287,
-        "pop_max": 1245000,
+        "pop_max": 1545000,
         "pop_min": 1021295
       }
     },
@@ -184,7 +184,7 @@ data = {
         "description": "Levantamento topográfico e inspeção visual subaquática em Saint Thomas, Ilhas Virgens Americanas",
         "latitude": 18.29802,
         "longitude": -64.9044384,
-        "pop_max": 1245000,
+        "pop_max": 1545000,
         "pop_min": 1021295
       }
     },
@@ -195,7 +195,7 @@ data = {
         "description": "Levantamento topográfico e inspeção visual subaquática em Saint Croix, Ilhas Virgens Americanas",
         "latitude": 17.7222475,
         "longitude": -64.7958542,
-        "pop_max": 1245000,
+        "pop_max": 1545000,
         "pop_min": 1021295
       }
     },
@@ -206,7 +206,7 @@ data = {
         "description": "Monitoramento da morfodinâmica costeira da Praia de Soledade, RN.",
         "latitude": -5.3050891,
         "longitude": -36.4960709,
-        "pop_max": 1245000,
+        "pop_max": 1545000,
         "pop_min": 1021295
       }
     }
